91b3f3ab0f81ef3411550fd94c2ac07926e9dfee
Chris Aragon <aragonchristopherray@gmail.com>
2020-06-22 18:32:10 -0400

    stamp router dropping all the things

--- src/Components/Stamp/Stamp.js
+++ src/Components/Stamp/Stamp.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
 
 export default class Stamp extends Component {
     render(){
+        console.log(this.props.Stamp_Dummy)
         return(
             <>
                 <header>
                     Nav Bar
                 </header>
                 <body>
-                    <h1>Parkname</h1><br/>
-                    <h3>Date Stamped: Date!</h3><br/>
+                    <h1>{this.props.Stamp_Dummy.fullName}</h1><br/>
+                    <h3>Date Stamped: {this.props.Stamp_Dummy.Date}</h3><br/>
                     <label for="comments">comments</label><br/>
-                    <p>This is a comment about my park experience!</p>
+                    <p>{this.props.Stamp_Dummy.comment}</p>
                 </body>
+                <Link to='/StampList'>Return to Your Stamps</Link>
                 <footer>
                     <p>Chris Aragon Footer baby!</p>
                 </footer>
