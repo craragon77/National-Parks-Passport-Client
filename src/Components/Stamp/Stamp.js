bfa159bb1a9fc5fec59a0c10b2eb531ce86b066f
Chris Aragon <aragonchristopherray@gmail.com>
2020-07-22 13:43:35 -0400

    need help with the mapping

--- src/Components/Stamp/Stamp.js
+++ src/Components/Stamp/Stamp.js
@@ -3,15 +3,11 @@ import {Link} from 'react-router-dom'
 
 export default class Stamp extends Component {
     render(){
-        console.log(this.props.Stamp_Dummy)
-        console.log(this.props.match.params.id)
+        console.log(this.props.stamps)
         return(
             <>
                 <body>
-                    <h1>{this.props.Stamp_Dummy[this.props.match.params.id-1].fullname}</h1><br/>
-                    <h3>Date Stamped: {this.props.Stamp_Dummy[this.props.match.params.id-1].Date}</h3><br/>
-                    <label for="comments">comments</label><br/>
-                    <p>{this.props.Stamp_Dummy[this.props.match.params.id-1].comment}</p>
+                    <p>{this.props.fullname}</p>
                 </body>
                 <Link to='/StampList'>Return to Stamp List</Link>
             </>
--- src/Components/StampList/StampList.js
+++ src/Components/StampList/StampList.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import Stamp from '../Stamp/Stamp';
 import './StampList.css';
 import StampFetchService from '../../Services/StampbookFetchService';
+import ParkFetchService from '../../Services/ParkFetchService';
 
 export default class StampList extends Component {
     constructor(props){
@@ -27,29 +28,45 @@ export default class StampList extends Component {
             })
             .catch(error => {console.log(error)})
     }
-    fetchPark = (id) => {
-        StampFetchService.getStampById(id)
+    /*fetchPark = (id) => {
+        ParkFetchService.getParkById(id)
             .then(res => {
                 if (res.ok){
-                    //ok so the thing unexpectedly ends here?
+                    //ok so i am getting a 401 unauthorized res here and idk why?
                     return res.json()
                 }
             })
             .then(resJson => {
-                 return console.log(resJson)
+                console.log(resJson.fullname)
+                //how can I take this mapped over resJson.fullname and make all the necessary list items or whatever?
+                return <Stamp fullname={resJson.fullname}/>
             })
             .catch((error) => {
                  console.log(console.log(error))
             })
             
-    }
+    } */
     
 
     render() {
         console.log(this.state.stamps)
-
         const stampArray = this.state.stamps.map(i => {
-            this.fetchPark(i.park_id)
+            //this.fetchPark(i.park_id)
+            ParkFetchService.getParkById(i.park_id)
+            .then(res => {
+                if (res.ok){
+                    //ok so i am getting a 401 unauthorized res here and idk why?
+                    return res.json()
+                }
+            })
+            .then(resJson => {
+                console.log(resJson.fullname)
+                //how can I take this mapped over resJson.fullname and make all the necessary list items or whatever?
+                return <Stamp fullname={resJson.fullname}/>
+            })
+            .catch((error) => {
+                 console.log(console.log(error))
+            })
         })
         return(
             <>
@@ -60,7 +77,7 @@ export default class StampList extends Component {
                         {this.props.Stamp_Dummy.map((stamp) => {
                             return(
                             <li key={stamp.id}>
-                                <p>placeholder</p>
+                                {stampArray}
                             </li>)
                         })
                     }
